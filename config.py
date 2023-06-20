a5936e050bbae1444b585f5864603c795f01beb4
Nayan <120218587+Nayannxd@users.noreply.github.com>
2023-06-20 08:11:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/56338e0dc4752813e8dbd.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/91b8d2e0484efd5b7d2b1.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://te.legra.ph/file/f820ab92cd67702b70e3e.jpg",
+    "https://te.legra.ph/file/733d7fe9be5c632b49397.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://te.legra.ph/file/f820ab92cd67702b70e3e.jpg"
+            PING_IMG_URL = "https://te.legra.ph/file/733d7fe9be5c632b49397.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://te.legra.ph/file/56338e0dc4752813e8dbd.jpg"
+            START_IMG_URL = "https://te.legra.ph/file/91b8d2e0484efd5b7d2b1.jpg"
